a4bb5f4024c3978ec868118c7455928ecb2b2199
Delan Azabani <delan@azabani.com>
2023-02-11 18:15:11 +0800

    chapter 3 base minus text styling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::{env::args, fmt::Debug, mem::swap, str};
 
+use ab_glyph::{Font, FontRef, PxScaleFont, ScaleFont};
 use egui::{
     pos2, vec2, Align, Align2, Color32, FontData, FontDefinitions, FontFamily, FontId, Frame,
-    Layout, Pos2, Rect, TextEdit, Ui, Vec2,
+    Layout, Pos2, Rect, Stroke, TextEdit, Ui, Vec2,
 };
 use tracing::{debug, error, instrument, trace};
 
@@ -10,10 +11,10 @@ use wbe::*;
 
 // to squelch rust-analyzer error on FONT_PATH in vscode, set
 // WBE_FONT_PATH to /dev/null in rust-analyzer.cargo.extraEnv
-const HSTEP: f32 = 13.0;
-const VSTEP: f32 = 18.0;
+const MARGIN: f32 = 16.0;
+const FONT_SIZE: f32 = 16.0;
 const FONT_NAME: &str = "Times New Roman";
-const FONT_PATH: &[u8] = include_bytes!(env!("WBE_FONT_PATH"));
+const FONT_DATA: &[u8] = include_bytes!(env!("WBE_FONT_PATH"));
 
 fn main() -> eyre::Result<()> {
     // log to stdout (level configurable by RUST_LOG=debug)
@@ -34,7 +35,7 @@ fn main() -> eyre::Result<()> {
             let mut font_definitions = FontDefinitions::default();
             font_definitions
                 .font_data
-                .insert(FONT_NAME.to_owned(), FontData::from_static(FONT_PATH));
+                .insert(FONT_NAME.to_owned(), FontData::from_static(FONT_DATA));
             font_definitions
                 .families
                 .get_mut(&FontFamily::Proportional)
@@ -60,7 +61,7 @@ struct Browser {
     location: String,
     document: Document,
     next_document: Document,
-    viewport_rect: Rect,
+    viewport: ViewportInfo,
     scroll: Vec2,
 }
 
@@ -71,7 +72,7 @@ impl Default for Browser {
             location: Default::default(),
             document: Default::default(),
             next_document: Default::default(),
-            viewport_rect: Rect::NAN,
+            viewport: Default::default(),
             scroll: Vec2::ZERO,
         }
     }
@@ -91,7 +92,7 @@ enum Document {
         location: String,
         response_body: String,
         display_list: Vec<PaintText>,
-        viewport_rect: Rect,
+        viewport: ViewportInfo,
     },
 }
 
@@ -101,7 +102,7 @@ impl Default for Document {
             location: "about:blank".to_owned(),
             response_body: "".to_owned(),
             display_list: vec![],
-            viewport_rect: Rect::NAN,
+            viewport: Default::default(),
         }
     }
 }
@@ -140,8 +141,8 @@ impl Document {
     fn size(&self) -> Vec2 {
         let mut result = Vec2::ZERO;
         if let Self::LaidOut { display_list, .. } = self {
-            for PaintText(position, ..) in display_list {
-                result = result.max(position.to_vec2());
+            for paint in display_list {
+                result = result.max(paint.rect().max.to_vec2());
             }
         }
 
@@ -150,36 +151,90 @@ impl Document {
 
     fn scroll_limit(&self) -> Vec2 {
         let mut result = self.size();
-        if let Self::LaidOut { viewport_rect, .. } = self {
-            result -= viewport_rect.size();
+        if let Self::LaidOut { viewport, .. } = self {
+            result -= viewport.rect.size();
         }
 
         result.max(Vec2::ZERO)
     }
 }
 
-#[derive(Debug)]
-struct PaintText(Pos2, FontId, String);
+#[derive(Debug, Clone)]
+struct PaintText(Rect, FontInfo, String);
 
 impl PaintText {
-    fn position(&self) -> &Pos2 {
+    fn rect(&self) -> &Rect {
         &self.0
     }
 
     fn font(&self) -> &FontId {
-        &self.1
+        &self.1.egui
     }
 
     fn text(&self) -> &str {
         &self.2
     }
+}
 
-    fn size(&self) -> Vec2 {
-        vec2(HSTEP, VSTEP)
+#[derive(Debug, Clone)]
+struct FontInfo {
+    egui: FontId,
+    ab: PxScaleFont<FontRef<'static>>,
+}
+
+impl FontInfo {
+    #[instrument(skip(data))]
+    fn new(
+        family: FontFamily,
+        data: &'static [u8],
+        size_egui_points: f32,
+        pixels_per_egui_point: f32,
+    ) -> eyre::Result<Self> {
+        let font_id = FontId::new(size_egui_points, family);
+
+        let font = FontRef::try_from_slice(data)?;
+        let ab_height_unscaled = font.height_unscaled();
+        let ab_units_per_em = font.units_per_em().expect("Font::units_per_em() was None");
+        let size_pixels =
+            size_egui_points * pixels_per_egui_point * ab_height_unscaled / ab_units_per_em;
+        trace!(ab_height_unscaled, ab_units_per_em);
+
+        Ok(Self {
+            egui: font_id,
+            ab: font.into_scaled(size_pixels),
+        })
     }
+}
 
-    fn rect(&self) -> Rect {
-        Rect::from_min_size(self.0, self.size())
+#[derive(Debug, PartialEq, Clone)]
+struct ViewportInfo {
+    rect: Rect,
+    scale: f32,
+}
+
+impl Default for ViewportInfo {
+    fn default() -> Self {
+        Self {
+            rect: Rect::NAN,
+            scale: f32::NAN,
+        }
+    }
+}
+
+impl ViewportInfo {
+    #[instrument(skip(self, cursor, screen_rect, pixels_per_point))]
+    fn update(&mut self, cursor: Rect, screen_rect: Rect, pixels_per_point: f32) -> &mut Self {
+        // e.g. cursor [[0 24] - [800 inf]], screen_rect [[0 0] - [800 600]]
+        let mut viewport_rect = cursor;
+        *viewport_rect.bottom_mut() = screen_rect.bottom();
+
+        if viewport_rect != self.rect || pixels_per_point != self.scale {
+            debug!(?cursor, ?screen_rect, pixels_per_point);
+            self.rect = viewport_rect;
+            self.scale = pixels_per_point;
+        }
+
+        self
     }
 }
 
@@ -203,45 +258,81 @@ impl Browser {
 
     #[instrument(skip(self, response_body))]
     fn layout(&mut self, location: String, response_body: String) -> eyre::Result<Document> {
-        let font = FontId::new(13.0, FontFamily::Proportional);
-        let viewport_rect = self.viewport_rect;
-        let x_min = viewport_rect.min.x + HSTEP;
-        let x_max = viewport_rect.max.x - HSTEP * 2.0;
-        let mut cursor = pos2(x_min, viewport_rect.min.y + HSTEP);
+        let viewport = self.viewport.clone();
+        let mut font = FontInfo::new(
+            FontFamily::Proportional,
+            FONT_DATA,
+            FONT_SIZE,
+            viewport.scale,
+        )?;
+        let mut font2 = FontInfo::new(
+            FontFamily::Proportional,
+            FONT_DATA,
+            FONT_SIZE * 1.25,
+            viewport.scale,
+        )?;
+        let x_min = viewport.rect.min.x + MARGIN;
+        let x_max = viewport.rect.max.x - MARGIN;
+        let mut cursor = pos2(x_min, viewport.rect.min.y + MARGIN);
         let mut input = &*response_body;
-        let mut display_list = vec![];
-        while let Some(token) = lparse_chomp(&mut input, "<.+?>|[^<]+") {
+        let mut display_list = Vec::<PaintText>::default();
+
+        // per-line data
+        let mut i = 0;
+        let mut max_ascent = 0.0f32;
+        let mut max_height = 0.0f32;
+
+        while let Some(mut token) = lparse_chomp(&mut input, r"<.+?>|[\t\n\v\r ]+|[^<]+") {
             if !token.starts_with("<") {
+                if lparse(token, r"[\t\n\v\r ]+").is_some() {
+                    token = " ";
+                }
                 for c in token.chars() {
-                    display_list.push(PaintText(cursor, font.clone(), c.to_string()));
-                    cursor += vec2(HSTEP, 0.0);
-                    if cursor.x > x_max {
+                    let glyph_id = font.ab.glyph_id(c);
+                    let advance = font.ab.h_advance(glyph_id) / viewport.scale;
+                    let ascent = font.ab.ascent() / viewport.scale;
+                    let height = font.ab.height() / viewport.scale;
+                    if cursor.x + advance > x_max {
+                        for paint in &mut display_list[i..] {
+                            *paint.0.top_mut() += max_ascent - paint.1.ab.ascent() / viewport.scale;
+                        }
                         cursor.x = x_min;
-                        cursor.y += VSTEP;
+                        cursor.y += max_height;
+                        i = display_list.len();
+                        max_ascent = 0.0;
+                        max_height = 0.0;
                     }
+                    max_ascent = max_ascent.max(ascent);
+                    max_height = max_height.max(height);
+                    let rect = Rect::from_min_size(cursor, vec2(advance, height));
+                    display_list.push(PaintText(rect, font.clone(), c.to_string()));
+                    cursor.x += advance;
+                    swap(&mut font, &mut font2);
                 }
             }
         }
-        debug!(display_item_count = display_list.len());
+        for paint in &mut display_list[i..] {
+            *paint.0.top_mut() += max_ascent - paint.1.ab.ascent() / viewport.scale;
+        }
+        trace!(display_list_len = display_list.len());
 
         Ok(Document::LaidOut {
             location,
             response_body,
             display_list,
-            viewport_rect,
+            viewport,
         })
     }
 
     #[instrument(skip(self, ui, display_list))]
-    fn paint(&self, ui: &Ui, display_list: &[PaintText], viewport_rect: &Rect) {
+    fn paint(&self, ui: &Ui, display_list: &[PaintText], viewport: &ViewportInfo) {
         let painter = ui.painter();
         for paint in display_list {
-            if paint
-                .rect()
-                .intersects(viewport_rect.translate(self.scroll))
-            {
+            let rect = paint.rect().translate(-self.scroll);
+            if rect.intersects(viewport.rect) {
+                painter.rect_stroke(rect, 0.0, Stroke::new(1.0, Color32::from_rgb(255, 0, 255)));
                 painter.text(
-                    *paint.position() - self.scroll,
+                    rect.min,
                     Align2::LEFT_TOP,
                     paint.text(),
                     paint.font().clone(),
@@ -253,7 +344,7 @@ impl Browser {
 
     #[instrument(skip(self))]
     fn tick(&mut self) -> eyre::Result<()> {
-        trace!(tick = self.tick);
+        // trace!(tick = self.tick);
         self.tick += 1;
 
         // debug!(
@@ -280,7 +371,7 @@ impl Browser {
 }
 
 impl eframe::App for Browser {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         if let Err(e) = self.tick() {
             error!("error: {}", e.to_string());
             panic!();
@@ -307,22 +398,25 @@ impl eframe::App for Browser {
             .show(ctx, |ui| {
                 // needed only for scroll_delta
                 egui::ScrollArea::both().show(ui, |ui| {
-                    self.viewport_rect = ui.cursor();
-                    *self.viewport_rect.bottom_mut() = ctx.screen_rect().bottom();
-
                     // FIXME minus to work around weird y direction
                     self.scroll -= ui.input(|i| i.scroll_delta);
                     self.scroll = self.scroll.clamp(Vec2::ZERO, self.document.scroll_limit());
 
                     if let Document::LaidOut {
                         display_list,
-                        viewport_rect,
+                        viewport,
                         ..
                     } = &self.document
                     {
-                        self.paint(ui, display_list, viewport_rect);
+                        self.paint(ui, display_list, viewport);
 
-                        if *viewport_rect != self.viewport_rect {
+                        if viewport
+                            != self.viewport.update(
+                                ui.cursor(),
+                                ctx.screen_rect(),
+                                ctx.pixels_per_point(),
+                            )
+                        {
                             let document = self.document.take().invalidate_layout();
                             if let Document::Loaded {
                                 location,
